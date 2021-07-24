196bf4ce1a37e8f6ed8a67343f9200f9a5564ac7
Kate Kolomiytsheva <copywriterkate88@gmail.com>
2021-07-24 20:16:21 +0300

    add at home 16.1

--- home16/script.js
+++ home16/script.js
@@ -1,1 +1,49 @@
-console.log(123)
+console.log(123);
+const dataTypes = [
+    true,
+    's',
+    0,
+    null,
+    undefined,
+    {},
+    [],
+    Symbol(),
+    () => {},
+]
+
+let map = new Map();
+
+for (let i = 0; i < dataTypes.length; i++) {
+
+    map.set(dataTypes[i], workForElementsOfarr(dataTypes[i]));
+
+}
+
+function workForElementsOfarr(arg) {
+    let objeckFoeElement = {};
+    objeckFoeElement['value'] = arg;
+    objeckFoeElement['type'] = typeof arg;
+    objeckFoeElement['typeString'] = String(arg);
+    objeckFoeElement['typeBoolean'] = Boolean(arg);
+
+
+    if (typeof arg !== typeof Symbol()) {
+        objeckFoeElement['typeNumber'] = Number(arg);
+    } else {
+        objeckFoeElement['typeNumber'] = '--'
+    }
+
+    return objeckFoeElement
+
+}
+
+console.log(map)
+
+map.forEach((valObj) => {
+    console.log("значение: " + String(valObj.value));
+    console.log("тип: " + valObj.type);
+    console.log("преобразование к числу:  " + valObj.typeNumber);
+    console.log("преобразование к строке: " + valObj.typeString);
+    console.log("преобразование к булеву: " + valObj.typeBoolean);
+
+})
