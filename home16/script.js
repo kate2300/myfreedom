4a64419861d2466792d3f95775db1577ef47bd19
Kate Kolomiytsheva <copywriterkate88@gmail.com>
2021-07-29 15:36:35 +0300

    home2

--- home16/script.js
+++ home16/script.js
@@ -47,3 +47,57 @@ map.forEach((valObj) => {
     console.log("преобразование к булеву: " + valObj.typeBoolean);
 
 })
+
+function countWords(text, checkWord) {
+    let counter = 0;
+    text.forEach((textWord) => {
+        if (textWord.toLowerCase() === checkWord) counter += 1;
+    });
+    return counter;
+}
+
+function calculateAnswers(text, testWords) {
+    const newAnswers = [];
+    testWords.forEach((elem) => {
+        newAnswers.push({
+            keyName: elem,
+            count: countWords(text, elem),
+        });
+    });
+    return newAnswers;
+}
+
+function convertToINNERHTNL(WORDS_OBJ) {
+    let finalText = '';
+    WORDS_OBJ.forEach((elem) => {
+        finalText = `${finalText} <br/>${elem.keyName} : ${elem.count}<br/>`;
+    });
+    return finalText;
+}
+
+function main() {
+    const div = document.querySelector('div');
+    const wrapper = document.createDocumentFragment();
+    const textArea = document.createElement('textarea');
+    const button = document.createElement('button');
+    const answers = document.createElement('div');
+
+    let testWords = [];
+
+    button.addEventListener('click', () => {
+        const textSliced = textArea.value.split(' ');
+        const mySet = new Set();
+        textSliced.forEach((elem) => { mySet.add(elem.toLowerCase()); });
+        testWords = calculateAnswers(textSliced, mySet);
+        answers.innerHTML = convertToINNERHTNL(testWords);
+    });
+
+    wrapper.appendChild(textArea);
+    wrapper.appendChild(button);
+    wrapper.appendChild(answers);
+
+    answers.innerHTML = convertToINNERHTNL(testWords);
+    div.append(wrapper);
+}
+
+main();
